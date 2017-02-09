69d589850c7cbd303112f4df8bc986a13d16698c
Serik Beysenov <serbeys@Seriks-MacBook-Pro.local>
2017-02-09 17:09:36 +0600

    firefox fix + resize event

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,4 @@
+var slider;
 
 document.addEventListener("DOMContentLoaded", function (event) {
         slider = new HNDSMSlider({
@@ -11,6 +12,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 );
 
+
+window.addEventListener('resize', function(event){
+    slider.changeHeight();
+});
+
 function HNDSMSlider(settings) {
     this.settings = settings;
     this.currentSlide = settings.startSlide;
@@ -19,27 +25,40 @@ function HNDSMSlider(settings) {
     this.slideHeight = this.slides[settings.startSlide].offsetHeight;
 
     this.start = function () {
-        TweenLite.set(this.slides, {y: this.slideHeight});
-        TweenLite.set(this.slides[this.settings.startSlide], {y: 0});
+        TweenLite.set(this.slides, isFirefox() ? {top: this.slideHeight} : {y: this.slideHeight});
+        TweenLite.set(this.slides[this.settings.startSlide], isFirefox() ? {top: 0} : {y: 0});
         this.timerFunction = TweenLite.delayedCall(this.settings.pause, changeSlide, [this]);
     };
 
+    this.changeHeight = function () {
+        this.currentSlide = this.settings.startSlide;
+        this.nextSlide = this.currentSlide + 1;
+        this.slideHeight = this.slides[settings.startSlide].offsetHeight;
+        this.start();
+    };
+
+    var isFirefox = function () {
+        return navigator.userAgent.search("Firefox") > -1;
+    };
+
+
     var changeSlide = function (slider) {
+
         TweenLite.to(slider.slides[slider.currentSlide], slider.settings.duration, {
-            css: {y: -(slider.slideHeight)},
+            css: isFirefox() ? {top: -(slider.slideHeight)} : {y: -(slider.slideHeight)},
             onComplete: onSlideComplete,
-            onCompleteParams:[slider, slider.slides[slider.currentSlide]],
+            onCompleteParams: [slider, slider.slides[slider.currentSlide]],
             ease: slider.settings.easing
         });
 
         TweenLite.to(slider.slides[slider.nextSlide], slider.settings.duration, {
-            css: {y: 0},
+            css: isFirefox() ? {top: 0} : {y: 0},
             ease: slider.settings.easing
         })
     };
 
     var onSlideComplete = function (slider, slide) {
-        TweenLite.set(slide, {y: slider.slideHeight});
+        TweenLite.set(slide, isFirefox() ? {top: slider.slideHeight} : {y: slider.slideHeight});
         slider.timerFunction.restart(true);
         slider.currentSlide = slider.nextSlide;
         slider.nextSlide++;
